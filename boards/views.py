fb6445659b7d3727ce2c09babee058f857e1e722
Knight <kebiguohong@sina.com>
2018-07-29 21:35:24 +0800

    last update

--- boards/views.py
+++ boards/views.py
@@ -3,24 +3,31 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Board, Topic, Post
 from .form import NewTopicForm, PostForm
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from django.views.generic import UpdateView, ListView
+from django.utils.decorators import method_decorator
+from django.db.models import Count
 # Create your views here.
 
-def home(request):
-	# boards = Board.objects.all()
-	# boards_names = list()
+class BoardListView(ListView):
+    model = Board
+    context_object_name = 'boards'
+    template_name = 'home.html'
 
-	# for board in boards:
-	# 	boards_names.append(board.name)
+class TopicListView(ListView):
+    model = Topic
+    context_object_name = 'topics'
+    template_name = 'topics.html'
+    paginate_by = 20
 
-	# response_html = '<br>'.join(boards_names)
+    def get_context_data(self, **kwargs):
+        kwargs['board'] = self.board
+        return super().get_context_data(**kwargs)
 
-	# return HttpResponse(response_html)
-	boards = Board.objects.all()
-	return render(request, 'home.html', {'boards': boards})
-
-def board_topics(request,pk):
-	board = get_object_or_404(Board, pk=pk)
-	return render(request, 'topics.html', {'board': board})
+    def get_queryset(self):
+        self.board = get_object_or_404(Board, pk=self.kwargs.get('pk'))
+        queryset = self.board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+        return queryset
 
 @login_required
 def new_topic(request, pk):
@@ -43,9 +50,23 @@ def new_topic(request, pk):
 
     return render(request, 'new_topic.html', {'board': board, 'form': form})
 
-def topic_posts(request, pk, topic_pk):
-    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
-    return render(request, 'topic_posts.html', {'topic': topic})
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'topic_posts.html'
+    paginate_by = 2
+
+    def get_context_data(self, **kwargs):
+        self.topic.views += 1
+        self.topic.save()
+        kwargs['topic'] = self.topic
+        return super().get_context_data(**kwargs)
+
+    def get_queryset(self):
+        self.topic = get_object_or_404(Topic, board__pk=self.kwargs.get('pk'), pk=self.kwargs.get('topic_pk'))
+        queryset = self.topic.posts.order_by('created_at')
+        return queryset
+
 
 @login_required
 def reply_topic(request,pk,topic_pk):
@@ -62,6 +83,26 @@ def reply_topic(request,pk,topic_pk):
         form = PostForm()
 
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
+
+@method_decorator(login_required, name='dispatch')
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('message', )
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_pk'
+    context_object_name = 'post'
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(created_by=self.request.user)
+
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.update_by = self.request.user
+        post.update_at = timezone.now()
+        post.save()
+        return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
 
 
 
